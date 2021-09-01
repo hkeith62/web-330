d0b705f1ce9d05c3b735105e0ab96edaf06ab6df
Keith Hall <hkeith62@gmail.com>
2021-09-01 08:21:56 -0500

    Code check.

--- week-3/appetizer.js
+++ week-3/appetizer.js
@@ -1,10 +1,3 @@
-/*
-    Title: WEB 330 – Enterprise JavaScript II
-    Author: Professor Itskovich
-    Date: 08/29/2021
-    Modified By: Keith Hall 
-    Description: Appetizer module that imports product class, extends, and exports .  
-*/
 import {Product} from "./product.js";
 
 export class Appetizer extends Product 
---
+++ week-4/calorie-converter.js
@@ -0,0 +1,11 @@
+import {FoodModel} from "./food-model.js"; 
+
+export class CalorieConverter extends FoodModel 
+{
+    constructor(name) {
+        this.name = name;
+    }
+    static(data) {
+        
+    }
+}
