cb889eec5a1a9014250cad6a95435c638dbf4bcb
Keith Hall <hkeith62@gmail.com>
2021-09-08 21:31:36 -0500

    Code check, comments, attribution.

--- week-5/shopping-cart.js
+++ week-5/shopping-cart.js
@@ -3,10 +3,28 @@
     Author: Professor Itskovich
     Date: 09/06/2021
     Modified By: Keith Hall 
-    Description: This is the exported shopping cart module.
+    Description: Exported shopping cart module.
 */
 export class ShoppingCart {
     constructor() {
-        productId = [];
+        this.productId = [];
     }
+        count() {  
+        return 
+    }
+        add(productName) {
+
+        }
+        *generatorFunction() {
+         yield "Oil Change"
+         yield "Filter Replacement"
+         yield "Wiper Fluid Refill"
+         } 
 }
+const myShoppingCart = new ShoppingCart();
+const generator = myShoppingCart.generatorFunction()
+// Iterate over Generator object
+for (const value of generator) {
+  console.log(value)
+}
+ 
