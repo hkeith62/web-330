de4b40114e27ebb16ba0fcebaa4b5b9d428c2dbc
Keith Hall <hkeith62@gmail.com>
2021-10-03 15:09:55 -0500

    Week-8.

--- personal-portfolio-website/main.js
+++
@@ -1,8 +0,0 @@
-<div id="container">
-    <div id="content-container" style="width:100%; height:740px">
-    
-    </div>
-    <div class= "divider">
-        <h2 class= "divider-text"></h2>
-    </div>
-</div>
--- week-8/http-client.js
+++ week-8/http-client.js
@@ -1,10 +1,17 @@
-export class HttpClient {  //a and 5
-    get = async (url, params = '') => {  // b
-        url = new URL(url); //1
-        url.search = new URLSearchParams(params); // 2
-        const res = await fetch(url.toString(), { //3
+/**
+    Title: WEB 330 – Enterprise JavaScript II
+    Author: Professor Itskovich
+    Date: 10/0s/2021
+    Modified By: Keith Hall 
+    Description: This is the float-field module for the Future Value App-web-330 assignment 6.2.   
+*/
+export class HttpClient {  
+    async get(url, params = '') {   
+        url = new URL(url);  
+        url.search = new URLSearchParams(params);  
+        const res = await fetch(url.toString(), {  
             method: 'GET'
         })
-        return res.json(); //4
+        return res.json(); 
     }
 }
