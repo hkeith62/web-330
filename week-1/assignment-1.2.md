9181470142211466c4de42e4a180883c1cf030cb
KEITH <keith@khprojectmanager.com>
2024-02-29 21:13:38 -0600

    New projects

--- week-1/assignment-1.2.md
+++
@@ -1,5 +0,0 @@
-# web-330
-## Contributors
-* Peter Itskovich
-* Keith Hall 
-This repository contains weekly assignments for web-330.
--- week-3/dessert.js
+++
@@ -1,9 +0,0 @@
-import {Product} from "./product.js";
-
-export class Dessert extends Product
-{
-	constructor(name, price)
-	{
-		super(name, price);
-	}
-}
